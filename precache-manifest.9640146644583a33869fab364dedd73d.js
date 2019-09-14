1d2469b90c2c9fd8135bf99850abf1df712d122a
bakurix <anthonysfeir9@gmail.com>
2019-09-15 01:37:51 +0300

    Updates

---
+++ precache-manifest.9640146644583a33869fab364dedd73d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "537a65723c8235b7a195f6f5c0a2c416",
+    "url": "/LiveChat/index.html"
+  },
+  {
+    "revision": "77b4478c72c8fa0cca37",
+    "url": "/LiveChat/static/css/main.c0cdeac3.chunk.css"
+  },
+  {
+    "revision": "c7e0b9e70a7f711c5d58",
+    "url": "/LiveChat/static/js/2.02c7bd87.chunk.js"
+  },
+  {
+    "revision": "77b4478c72c8fa0cca37",
+    "url": "/LiveChat/static/js/main.d28fcc30.chunk.js"
+  },
+  {
+    "revision": "356c55217d8f15b4ec0d",
+    "url": "/LiveChat/static/js/runtime~main.dc14cc9b.js"
+  }
+]);
