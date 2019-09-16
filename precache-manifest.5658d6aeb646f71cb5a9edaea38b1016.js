fe70587e34ad293edde43cab1c8b046af09dc2c0
bakurix <anthonysfeir9@gmail.com>
2019-09-17 02:00:41 +0300

    Updates

---
+++ precache-manifest.5658d6aeb646f71cb5a9edaea38b1016.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7adcc34057df61b3a2a3c89559d9ee38",
+    "url": "/ChatApp/index.html"
+  },
+  {
+    "revision": "4e2b096e1d78e3ddfca4",
+    "url": "/ChatApp/static/css/main.3fb98e01.chunk.css"
+  },
+  {
+    "revision": "40ef2f5312762b8b1a88",
+    "url": "/ChatApp/static/js/2.d734336d.chunk.js"
+  },
+  {
+    "revision": "4e2b096e1d78e3ddfca4",
+    "url": "/ChatApp/static/js/main.90ddfdc8.chunk.js"
+  },
+  {
+    "revision": "dda62a1bde1228519945",
+    "url": "/ChatApp/static/js/runtime~main.7e460a6d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/LiveChat/precache-manifest.9640146644583a33869fab364dedd73d.js"
+  "/ChatApp/precache-manifest.5658d6aeb646f71cb5a9edaea38b1016.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/LiveChat/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ChatApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
